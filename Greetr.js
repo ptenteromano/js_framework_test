ae6e01ff0883e08f5eba489c9e2bf0815343c114
Philip Tenteromano <tenteromano.p@gmail.com>
2018-07-11 17:26:44 -0400

    comment the code

--- Greetr.js
+++ Greetr.js
@@ -1,12 +1,15 @@
 // js framework to build JS skills
 
-//
+// IIFE - hides the code within the scope of the framework's closure
+// immediately invoked function expression.. note (.., ..) at end
 (function (global, $) {
 
+  // 'new's an object 
   var Greetr = function(firstName, lastName, language) {
     return new Greetr.init(firstName, lastName, language);
   }
 
+  // objects used in method calls, hidden within scope of IIFE
   var supportedLangs = ['en', 'es'];
 
   var greetings = {
@@ -24,26 +27,36 @@
     es: 'Inicio Sesion'
   }
 
+  // prototype holds methods -- saving memory space
+  // all Greetr objects have access to these methods!
+  // prototypes rock
   Greetr.prototype = {
 
+    // 'this' always refers to calling object at runtime
     fullName: function() {
       return this.firstName + ' ' + this.lastName;
     },
 
+    // references supportedLangs array accessible within closure
     validate: function() {
       if (supportedLangs.indexOf(this.language) === -1) {
         throw "Invalid Language";
       }
     },
 
+    // using [] syntx to access properties within the object
+    // note the difference between function greeting and obj greetings
     greeting: function() {
       return greetings[this.language] + ' ' + this.firstName + '!';
     },
 
+    // see above
     formalGreeting: function() {
       return formalGreetings[this.language] + ' ' + this.fullName();
     },
 
+    // chainable methods use
+    // return 'this';
     greet: function(formal) {
       var msg;
 
@@ -65,6 +78,8 @@
     },
 
     log: function() {
+      // internet explorer only has 'console' when it is opened
+      // this prevents error
       if (console) {
         console.log(logMessages[this.language] + ': ' + this.fullName());
       }
@@ -86,6 +101,7 @@
       if (!selector) {
         throw 'missing jQuery selector!';
       }
+
       var msg;
       if (formal) {
         msg = this.formalGreeting();
@@ -94,6 +110,7 @@
         msg = this.greeting();
       }
 
+      // inject message into selected place in the DOM
       $(selector).html(msg);
 
       return this;
@@ -102,17 +119,23 @@
 
   }; // end prototype
 
+  // actual object created here, allowing us to 'new' an object without
+  // actually calling 'new' everywhere
   Greetr.init = function(firstName, lastName, language) {
     var self = this;
     self.firstName = firstName || '';
     self.lastName = lastName || '';
     self.language = language || 'en';
+
+    self.validate();
   }
 
-  // point all
+  // point all .init object to library prototype functions
+  // saves us from using the 'new' keyword
   Greetr.init.prototype = Greetr.prototype
 
-  // alias the name Greetr to G$
+  // attached to global object, also alias the name Greetr to G$
   global.Greetr = global.G$ = Greetr
 
+// below, the IIFE is invoked and executed passing in these parameters
 }(window, jQuery));
